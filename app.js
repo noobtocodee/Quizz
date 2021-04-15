c85053eaeb5b25cd56451e65bbffffd364659322
noobtocodee <ivikasyadav7@gmail.com>
2021-04-15 22:26:56 +0530

    1st commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var nameSubmit = document.querySelector("#nameSubmit")
+
+// function clickHandler(){
+//     var inputText = txtInput.value;
+//     console.log("welcome")
+// }
+function showInput() {
+    var x = document.getElementById("showInput").value;
+}
